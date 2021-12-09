08e74d3f3cba1f7094d0d610b8f72dc5c4c2d981
sakura <sakura@824.com>
2021-12-09 16:44:46 +0800

    eurekamain first

--- payment8001/src/main/java/com/springcloud/controller/PaymentController.java
+++ payment8001/src/main/java/com/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.springcloud.entities.CommonResult;
 import com.springcloud.entities.Payment;
 import com.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -19,7 +16,7 @@ public class PaymentController {
 
     //写操作
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info(""+result);
         if(result > 0){
