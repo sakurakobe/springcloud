d1d396bcc0c0638079354ed9fb8ff96d9953f37a
sakura <sakura@824.com>
2021-12-12 13:53:28 +0800

    discovery first

--- consumer80/src/main/java/com/springcloud/config/ApplicationContextConfig.java
+++ consumer80/src/main/java/com/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +11,9 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class ApplicationContextConfig {
-
+//使用注解赋予RestTemplate负载均衡的能力
     @Bean
+    @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- payment8001/src/main/java/com/springcloud/controller/PaymentController.java
+++ payment8001/src/main/java/com/springcloud/controller/PaymentController.java
@@ -4,9 +4,13 @@ import com.springcloud.entities.CommonResult;
 import com.springcloud.entities.Payment;
 import com.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -14,13 +18,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
     //写操作
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info(""+result);
         if(result > 0){
-            return new CommonResult(200,"插入成功",result);
+            return new CommonResult(200,"插入成功"+serverPort,result);
         }else{
             return new CommonResult(404,"失败",null);
         }
@@ -32,9 +41,22 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info(""+payment);
         if(payment != null){
-            return new CommonResult(200,"查询成功",payment);
+            return new CommonResult(200,"查询成功"+serverPort,payment);
         }else{
             return new CommonResult(404,"失败"+id,null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services){
+            log.info("element"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("payment-service");
+        for(ServiceInstance instance : instances){
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
